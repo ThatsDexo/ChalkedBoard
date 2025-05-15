7b9dbce7380188f750361e848262acbb1649360d
ThatsDexo <kfwarner@eiu.edu>
2025-05-15 17:43:33 -0500

    created form

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chalked Board</title>
+</head>
+<body>
+    <form id="user-form" method= "post" action="stickynote.php"> 
+    <input type="text" id="message" name="message" placeholder="Enter your message">
+    <input type="text" id="name" name ="name" placeholder="Enter your name"> 
+    </form>
+</body>
+</html>
