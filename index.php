3e19061029e18dbba6e6d1656bcd11df4d99fd03
ThatsDexo <kfwarner@eiu.edu>
2025-05-15 22:48:22 -0500

    background image styling

--- index.php
+++ index.php
@@ -4,11 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chalked Board</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <form id="user-form" method= "post" action="stickynote.php"> 
-    <input type="text" id="message" name="message" placeholder="Enter your message">
-    <input type="text" id="name" name ="name" placeholder="Enter your name"> 
-    </form>
+    <section>
+        <div>
+            <form id="user-form" method= "post" action="stickynote.php"> 
+            <input type="text" id="message" name="message" placeholder="Enter your message">
+            <input type="text" id="name" name ="name" placeholder="Enter your name"> 
+            </form>
+        </div>
+    </section>
 </body>
 </html>
